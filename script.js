784cd4831c8f0074dce6a663428734e7fb1baefb
Elita Chan <elitachan03@gmail.com>
2023-09-14 15:06:33 -0400

    Create divs for each square when start game

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ const startFormBtn = document.querySelector(".startFormBtn");
 const playerForm = document.getElementById("playerForm");
 const overlay = document.querySelector(".overlay");
 
+const newNode = (square, index) => {
+    const gameSquare = document.createElement("div");
+    gameSquare.classList.add("square");
+    gameSquare.setAttribute("id", `square-${index}`);
+    gameSquare.textContent = `${square}`;
+    return gameSquare;
+}
+
+const GameBoard = (() => {
+    let gameboard = ["", "", "", "", "", "", "", "", ""];
+
+    const render = () => {
+        gameboard.forEach((square, index) => {
+            document.querySelector(".gameBoard").appendChild(newNode(square, index));
+        });
+    }
+
+    return {
+        render,
+
+    }
+})();
+
 startFormBtn.addEventListener("click", () => {
     startFormBtn.classList.add('active');
     playerForm.classList.add('active');
@@ -11,5 +34,5 @@ playerForm.addEventListener("submit", (e) => {
     e.preventDefault();
     playerForm.classList.remove('active');
     overlay.classList.remove('active');
+    GameBoard.render();
 })
-
